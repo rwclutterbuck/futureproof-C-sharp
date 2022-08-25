78fccb3b95363e211a2fd8682a8e3f77ca6cd9a8
rwclutterbuck <rwclutterbuck@outlook.com>
2022-08-25 13:38:42 +0100

    day 1: intro

---
+++ simple/MyCode.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCode
+{
+    class MyCode
+    {
+	   public static int Sum(int a, int b)
+	   {
+	       return a + b;
+	   }
+    }
+}
---
+++ simple/Program.cs
@@ -0,0 +1,39 @@
+﻿
+// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+//
+// int a = 10;
+// Console.WriteLine("The number is {0}", a);
+// Console.WriteLine($"The number is {a}");
+//
+// int b = 3;
+// int c = ++b;
+//
+// Console.WriteLine($"b is {b} c is {c}");
+//
+// // for (int i = 0; i < 10; i++)
+// // {
+// //     Console.WriteLine($"{i}");
+// // }
+//
+// Console.WriteLine(sizeof(Int64));
+
+using MyCode;
+
+Console.WriteLine("What is you age?");
+string? input = Console.ReadLine();
+try
+{
+    int age = int.Parse(input);
+    Console.WriteLine($"You are {age} years old");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("You age is a valid format but it is either to big or too small");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+
+Console.WriteLine(MyCode.MyCode.Sum(1,3));
